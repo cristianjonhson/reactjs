e3522afe957b73a5caed63e4b48c41d4bf8cb66d
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-01-09 00:14:37 -0300

    Ejemplo de reutilización de componentes con props (Saludo)

--- 00--holamundo/src/App.jsx
+++ 00--holamundo/src/App.jsx
@@ -3,12 +3,16 @@
 import './App.css';
 import Mensaje from './Mensaje';
 import Boton from './Boton';
+import Saludo from './Saludo';
 
 export default function App() {
   return (
     <div className="app-container">
       <h1>¡Hola Mundo desde React + Vite!</h1>
       <Mensaje />
+      <Saludo nombre="Cristian" />
+      <Saludo nombre="React" />
+      <Saludo nombre="Mundo" />
       <Boton />
     </div>
   );
