a8ac5955e8752a83aa91fd87024d991c71e27a3d
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-01-08 23:35:28 -0300

    Segundo componente Boton

--- 00--holamundo/src/App.jsx
+++ 00--holamundo/src/App.jsx
@@ -1,10 +1,13 @@
+
 import Mensaje from './Mensaje';
+import Boton from './Boton';
 
 export default function App() {
   return (
     <>
       <h1>¡Hola Mundo desde React + Vite!</h1>
       <Mensaje />
+      <Boton />
     </>
   );
 }
---
+++ 00--holamundo/src/Boton.jsx
@@ -0,0 +1,3 @@
+export default function Boton() {
+  return <button onClick={() => alert('¡Has hecho clic!')}>Haz clic</button>;
+}
